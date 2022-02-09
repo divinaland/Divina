dd2cc79ead15a6c147c2dd7d0513e08efbc8280f
Fuwn <fuwnzy@gmail.com>
2022-02-08 17:27:52 -0800

    chore(xamples/greet): rename global

--- examples/greet/Main.asm
+++ examples/greet/Main.asm
@@ -2,13 +2,13 @@ global main
 extern printf
 
 section .rodata
-msg: db "Hello World!", 13, 10, 0
+message: db "Hello World!", 13, 10, 0
 
 section .text
 main:
   sub  rsp,   8
   sub  rsp,   32
-  mov  rcx,   qword msg
+  mov  rcx,   qword message
   call printf
   add  rsp,   32
 
